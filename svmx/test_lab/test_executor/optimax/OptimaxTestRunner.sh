d9e480cdd67bd98cb634b19d2f5d967c9206dab5
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2017-08-21 10:20:31 +0530

    New folder for test executor created

---
+++ svmx/test_lab/test_executor/optimax/OptimaxTestRunner.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+echo "Taking you to test runner work space."
+
+cd /auto/sahi_pro/userdata/bin
+./testrunner.sh Sahi_Project/svmx/test_lab/test_suites/optimax/optimax.suite http://sahitest.com/demo/ chrome
